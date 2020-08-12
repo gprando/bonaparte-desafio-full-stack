69662d06027fcec35f177f1f63c3783463818d39
Gabriel Prando <gprando55@gmail.com>
2020-08-12 09:41:09 -0300

    Update zip.js

--- zip.js
+++ zip.js
@@ -22,3 +22,55 @@ const baixar = (url) => {
 };
 
 baixar('https://datasets.imdbws.com/title.ratings.tsv.gz');
+
+
+// certo !
+
+const { createWriteStream } = require('fs');
+const axios = require('axios');
+const fs = require('fs')
+const zlib = require('zlib')
+
+const gunzip = zlib.createGunzip()
+
+
+
+async function downloadFile(fileUrl, outputLocationPath) {
+  const writer = createWriteStream(outputLocationPath);
+
+  return axios({
+    method: 'get',
+    url: fileUrl,
+    responseType: 'stream',
+  }).then(response => {
+
+    //ensure that the user can call `then()` only when the file has
+    //been downloaded entirely.
+
+    return new Promise((resolve, reject) => {
+      response.data.pipe(writer);
+      let error = null;
+      writer.on('error', err => {
+        error = err;
+        writer.close();
+        reject(err);
+      });
+      writer.on('close', () => {
+        if (!error) {
+          resolve(true);
+        }
+        //no need to call the reject here, as it will have been called in the
+        //'error' stream;
+    
+      });
+    });
+  });
+}
+
+downloadFile('https://datasets.imdbws.com/title.basics.tsv.gz', './data.tsv.gz').then(res=>{
+  const inFile = fs.createReadStream('./data.tsv.gz')
+  const out = fs.createWriteStream('./data.tsv');
+
+  inFile.pipe(gunzip).pipe(out)
+})
+
